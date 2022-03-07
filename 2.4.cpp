817c175d74aaa6bfc683b14e4ac53681cb0a7962
Евгений Шеленков <91319891+de3eliroli@users.noreply.github.com>
2022-03-07 18:39:26 +0500

    Add files via upload

---
+++ 2.4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int spaces = n / 2;
+	int stars = 1;
+	if ((n > 3) and (n % 2 != 0)) {
+		for (int i = 1; i <= (n + 1) / 2; i++) {
+			for (int j = 1; j <= spaces; j++) {
+				cout << " ";
+			}
+			spaces--;
+			for (int j = 1; j <= stars; j++) {
+				cout << "*";
+			}
+			stars += 2;
+			cout << endl;
+		}
+	}
+	return 0;
+
+}
